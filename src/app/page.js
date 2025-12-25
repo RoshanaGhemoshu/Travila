679de0665d8361e1a28d8bbfa5e40bf504ef5aab
Roshana <rosnaghemoshu@gmail.com>
2025-12-25 19:28:45 +0545

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -939,7 +939,7 @@ export default function TravilaWebsite() {
       {/* Left Column - Text Content (Takes 2 columns) */}
       <div className="lg:col-span-2 pl-95">
         {/* Client Avatars */}
-        <div className="flex items-center space-x-3 mb-6 pl-30">
+        <div className="flex items-center space-x-3 mb-6 pr-30">
           <div className="flex items-center">
             <div className="w-10 h-10 bg-white rounded-full border-2 border-white shadow-sm overflow-hidden">
               <img
