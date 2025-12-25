204e8e4aee9922f8bd9f372f143ab6f014a0e81c
Roshana <rosnaghemoshu@gmail.com>
2025-12-25 18:58:45 +0545

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -271,7 +271,7 @@ export default function TravilaWebsite() {
 {/* Main Content */}
 <div className="w-full px-4 sm:px-6 lg:px-8">
   {/* Hero Title */}
-  <div className="mb-8 sm:mb-12 flex flex-col sm:flex-row items-start justify-between gap-4 pr-20 mt-10">
+  <div className="mb-8 sm:mb-12 flex flex-col sm:flex-row items-start justify-between gap-4 pl-50 mt-10">
   <div >
     <h1 className="text-3xl sm:text-5xl lg:text-6xl text-black font-bold mb-3 leading-tight">
       Your Journey, Your Way
