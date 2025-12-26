6fe5c6761d3be27815d2dfd563d21e7edfc82124
Roshana <rosnaghemoshu@gmail.com>
2025-12-26 17:16:18 +0545

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -203,7 +203,7 @@ export default function TravilaWebsite() {
         </header>
 {/* Search Section */}
 <div className="bg-white py-4 border-b mt-5">
-  <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+  <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
     {/* Search Bar with Integrated Buttons */}
     <div className="bg-white rounded-full border border-gray-300 flex items-stretch mb-3 overflow-hidden">
       {/* Search Input - Gray Background - Takes more space */}
@@ -212,7 +212,7 @@ export default function TravilaWebsite() {
         <input
           type="text"
           placeholder="What are you looking for?"
-          className="flex-1 outline-none text-gray-900 text-s bg-transparent py-2 placeholder:text-[#737373]"
+          className="flex-1 outline-none text-gray-900 text-xs bg-transparent py-2 placeholder:text-[#737373]"
         />
       </div>
       
